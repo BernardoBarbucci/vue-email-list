abf6bbaf492df513b542b7430fa20033817c2715
“Bernardo <bernardobarbucci@gmail.com>
2023-12-12 16:44:58 +0100

    test con methods e created per richiamarlo

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            email: '',
         }
     },
 
     methods: {
+        getNewMail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(
+                (response) =>{
+                console.log(response);
+                this.email = response.data.response;
+        }
+    )},
 
-    }
-
+    created() {
+        this.getNewMail();
+    },
 }).mount('#app');
