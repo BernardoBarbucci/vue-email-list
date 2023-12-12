f73a8895867977c0d945029a30133029204de325
“Bernardo <bernardobarbucci@gmail.com>
2023-12-12 16:49:56 +0100

    errore di sintassi risolto, codice funzionante

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            email: '',
-        }
+        email: '',
+        };
     },
 
     methods: {
         getNewMail() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(
-                (response) =>{
-                console.log(response);
-                this.email = response.data.response;
-        }
-    )},
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+            console.log(response);
+            this.email = response.data.response;
+            })
+        },
+    },
 
     created() {
         this.getNewMail();
